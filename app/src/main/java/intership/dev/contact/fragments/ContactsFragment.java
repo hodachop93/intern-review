0d660a70f8277eea5c838bd2ae5060d6517daa7f
Ho Dac Hop <hodachop93@gmail.com>
2015-07-21 17:18:39 +0700

    Update ContactsLisAdapter.java

--- app/src/main/java/intership/dev/contact/fragments/ContactsFragment.java
+++ app/src/main/java/intership/dev/contact/fragments/ContactsFragment.java
@@ -1,5 +1,7 @@
 package intership.dev.contact.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -7,24 +9,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import intership.dev.contact.R;
+import intership.dev.contact.model.User;
 import intership.dev.contact.utils.ActionBarCustom;
+import intership.dev.contact.utils.ContactListAdapter;
 
 /**
  * This class is used to display list of contacts
  * Created by hodachop93 on 21/07/2015.
  */
 public class ContactsFragment extends Fragment {
-    //Action Bar
     private ActionBarCustom mActionBar;
 
     //Listview is used to display a list of contacts
     private ListView mListView;
+
+    //The list of users
+    private List<User> mUsers;
+
+    //The adapter of the listview
+    private ContactListAdapter mAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView =inflater.inflate(R.layout.fragment_contacts, container, false);
         mActionBar = (ActionBarCustom) rootView.findViewById(R.id.actionbar);
         mListView = (ListView) rootView.findViewById(R.id.lvContacts);
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mUsers = new ArrayList<User>();
+        for (int i=0;i<20;i++){
+            Bitmap avatar = BitmapFactory.decodeResource(getResources(),R.drawable.ic_avt1);
+            User user = new User(avatar,"HoDacHop","");
+            mUsers.add(user);
+        }
+        mAdapter = new ContactListAdapter(getActivity(), mUsers);
+        mListView.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/intership/dev/contact/utils/ContactListAdapter.java
+++ app/src/main/java/intership/dev/contact/utils/ContactListAdapter.java
@@ -1,7 +1,124 @@
 package intership.dev.contact.utils;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import intership.dev.contact.R;
+import intership.dev.contact.model.User;
+
 /**
  * Created by hodachop93 on 21/07/2015.
  */
-public class ContactListAdapter {
+public class ContactListAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<User> mUsers;
+
+    /**
+     * Create an adapter for a ListView that contains a list of contacts
+     *
+     * @param mContext The context that adapter is running
+     * @param mUsers   The collection of users
+     */
+    public ContactListAdapter(Context mContext, List<User> mUsers) {
+        this.mUsers = mUsers;
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return mUsers.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mUsers.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_list_contacts, parent, false);
+
+            holder = new ViewHolder();
+            holder.mAvatar = (ImageView) convertView.findViewById(R.id.imgAvatar);
+            holder.mUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+            holder.mImgBtnEdit = (ImageButton) convertView.findViewById(R.id.imgBtnEdit);
+            holder.mImgBtnDelete = (ImageButton) convertView.findViewById(R.id.imgBtnDelete);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        setValue(holder, position);
+        setEvent(holder, position);
+        return convertView;
+    }
+
+    /**
+     * Set events for a ViewHolder corresponding to a user in list contacts
+     *
+     * @param holder   The viewholder in the current convert view
+     * @param position The position of a user in list of users corresponding
+     *                 to the position of an item in ListView
+     */
+    private void setEvent(ViewHolder holder, final int position) {
+        User user = (User) getItem(position);
+
+        holder.mImgBtnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "edit" + position + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.mImgBtnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "delete" + position + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * Set value for a ViewHolder corresponding to a user in list contacts
+     *
+     * @param holder   The viewholder in the current convert view
+     * @param position The position of a user in list of users corresponding
+     *                 to the position of an item in ListView
+     */
+    private void setValue(ViewHolder holder, int position) {
+        User user = (User) getItem(position);
+
+        holder.mAvatar.setImageBitmap(user.getAvatar());
+        holder.mUsername.setText(user.getUserName());
+
+    }
+
+    /**
+     * This class is used to control view items in convertview
+     */
+    private static class ViewHolder {
+        ImageView mAvatar;
+        TextView mUsername;
+        ImageButton mImgBtnEdit;
+        ImageButton mImgBtnDelete;
+    }
 }
